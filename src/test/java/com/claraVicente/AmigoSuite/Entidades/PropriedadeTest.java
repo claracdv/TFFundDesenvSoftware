ba9cf8dc4cf7d3ea96e07d6af9b63e398e76ef94
claracdv <clara.voy@hotmail.com>
2020-06-28 21:06:30 -0300

    adicionando pasta resources e drivers de teste

--- src/test/java/com/claraVicente/AmigoSuite/Entidades/PropriedadeTest.java
+++ src/test/java/com/claraVicente/AmigoSuite/Entidades/PropriedadeTest.java
@@ -1,5 +1,15 @@
 package com.claraVicente.AmigoSuite.Entidades;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class PropriedadeTest {
-    
+    private Propriedade prop;
+
+    @BeforeEach
+    public void setup(){
+        prop = new Propriedade(id, custo, cidade, bairro, cep, endereco, classificacao);
+    }
+
 }
